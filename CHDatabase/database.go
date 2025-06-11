c715c3f06eecb2d23a4eae2a781f1df002ce06d6
Peter <peter.chiang@phofuture.com>
2025-06-11 16:32:20 +0800

    [feat] update module and sync core service

--- CHDatabase/database.go
+++ CHDatabase/database.go
@@ -1,12 +1,14 @@
-package CHDatabase
+package chdatabase
 
 import (
 	"context"
 
+	"github.com/ClickHouse/clickhouse-go/v2"
 	"gorm.io/gorm"
 )
 
 var masterDb, slaveDb *gorm.DB
+var clickConn clickhouse.Conn
 
 // 取得 master database
 func Master(ctx context.Context) *gorm.DB {
@@ -16,4 +18,8 @@ func Master(ctx context.Context) *gorm.DB {
 // 取得 slave database
 func Slave(ctx context.Context) *gorm.DB {
 	return slaveDb.WithContext(ctx)
+}
+
+func Conn() clickhouse.Conn {
+	return clickConn
 }
